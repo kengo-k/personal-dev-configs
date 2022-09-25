60265c144deae9c0e7e471dec3409691e0acd621
kengo-k <kurobanekengo@gmail.com>
2022-09-25 17:45:58 +0900

    install-mac.sh: OS別の対応を入れる

--- vscode/install-mac.sh
+++ vscode/install-mac.sh
@@ -2,8 +2,24 @@
 
 echo "> setup vscode settings.json..."
 
-SETTINGS_DIR="$HOME/Library/Application Support/Code/User"
+WIN_DIR="$HOME/AppData/Roaming/Code/User"
+MAC_DIR="$HOME/Library/Application Support/Code/User"
+LINUX_DIR="$HOME/.config/Code/User"
+
+SETTINGS_DIR=
 SETTINGS_FILE=settings.json
+
+case ${OSTYPE} in
+  darwin*)
+    SETTINGS_DIR="$MAC_DIR"
+    ;;
+  linux*)
+    SETTINGS_DIR="$LINUX_DIR"
+    ;;
+  msys*)
+    SETTINGS_DIR="$WIN_DIR"
+    ;;
+esac
 
 if [ -f "$SETTINGS_DIR/$SETTINGS_FILE" ]; then
   time=$(date +%Y%m%d%H%M%S)
@@ -17,5 +33,11 @@ CURRENT_DIR=$(
   cd $(dirname $0)
   pwd
 )
+
+echo "cd \"$SETTINGS_DIR\""
 cd "$SETTINGS_DIR"
+
+# Windows(Git Bash)の場合はlnしてもシンボリックリンクは作成されずに単にコピーされる
+# 対応するには設定と管理者権限でGit Bashを動かす必要がある模様(面倒なので対応しない)
+echo "ln -s $CURRENT_DIR/settings.json ./$SETTINGS_FILE"
 ln -s $CURRENT_DIR/settings.json ./$SETTINGS_FILE
